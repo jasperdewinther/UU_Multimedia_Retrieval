ade8d814b52ed146a72cee2aa3b00e9a570d6b7c
jasperdewinther <jaspersophie10@gmail.com>
2022-09-16 12:29:18 +0200

    added datasets and initial drawing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import trimesh
+import pyrender
 
 
+fuze_trimesh = trimesh.load('assets/sheep.obj')
+mesh = pyrender.Mesh.from_trimesh(fuze_trimesh)
+scene = pyrender.Scene()
+scene.add(mesh)
+pyrender.Viewer(scene, use_raymond_lighting=True)
 
-
-
-
-print("heey")
+# add recursive obj file finder
+# add multiple model viewer (instead of only showing one), to see top n results
+# add buttons to gui
