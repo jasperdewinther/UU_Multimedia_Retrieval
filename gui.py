198abce37a9268f97685b9f18cb0d9c1fd7f4d57
ArjenSimons <arjen.simons@gmail.com>
2022-09-21 15:29:13 +0200

    Add basic UI window

---
+++ gui.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg;
+import mesh_io
+
+def initGUI():
+    sg.theme('DarkAmber')   # Add a touch of color
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Filepath')],
+            [sg.Input(), sg.FileBrowse()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+    # Create the Window
+    window = sg.Window('MMR', layout)
+    return window
+
+def HandleGUIEvents(window):
+    event, input = window.read() 
+    print(event, input)   
+    model = mesh_io.get_mesh(input)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        return False  
+
+    return True
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from asyncio import Handle
+from gui import HandleGUIEvents, initGUI
 import mesh_io
 import renderer
 import filter_io
+import PySimpleGUI as sg
+
 
 mesh_files = mesh_io.get_all_obj_files("./assets/")
 meshes = mesh_io.get_all_meshes(mesh_files)
@@ -8,4 +12,13 @@ meshes = mesh_io.get_all_meshes(mesh_files)
 filter_io.get_class(mesh_files)
 filter_io.get_faces_vertices(mesh_files)
 
-renderer.render_meshes(meshes[5:20])
+renderer.render_meshes([meshes[6]])
+
+window = initGUI()
+
+while True:                             # The Event Loop
+    if not HandleGUIEvents(window):
+        break
+
+
+window.close()
