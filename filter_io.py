6a4a217f4c28f1928c60c3cd6302eac168e2e280
sheesakkers <sannaheesakkers@hotmail.com>
2022-09-21 22:07:50 +0200

    Fixed 2.1

--- filter_io.py
+++ filter_io.py
@@ -1,6 +1,7 @@
 import trimesh
 import os
 import csv
+import numpy as np
 
 def get_class(mesh_files):
     # finds the the class of the shape (for sheep, etc this is "assets")
@@ -17,12 +18,41 @@ def get_faces_vertices(mesh_files):
         mesh = trimesh.load_mesh(meshes)
         vertices = mesh.vertices.shape[0]
         faces = mesh.faces.shape[0]
+        trimesh.Scene().bounds
         #writer.writerow([vertices, faces])
     f.close()
     
+def get_face_type(mesh_files):
+    # find the types of faces
+    for meshes in mesh_files:
+        triangles = False
+        quads = False
+        f = open(meshes, 'r')
+        for lines in f:
+            lines_array = lines.split()
+            if len(lines_array) > 0:
+                if lines_array[0] == 'f':
+                    if len(lines_array) == 4: triangles = True
+                    elif len(lines_array) == 5: quads = True
+                if triangles == True and quads == True: break
 
-def get_face_type():
-    return
-
-def get_bounding_box():
-    return
+def get_bounding_box(mesh_files):
+    # find bounding box of the shape
+    for meshes in mesh_files:
+        bounding_box = [np.inf, np.inf, np.inf, -np.inf, -np.inf, -np.inf] #[x_min, y_min, z_min, x_max, y_max, z_max]
+        f = open(meshes, 'r')
+        for lines in f:
+            lines_array = lines.split()
+            if len(lines_array) > 0:
+                if lines_array[0] == 'v':
+                    vertices = []
+                    vertices.append(float(lines_array[1]))
+                    vertices.append(float(lines_array[2]))
+                    vertices.append(float(lines_array[3]))
+                    if bounding_box[0] > vertices[0]: bounding_box[0] = vertices[0]
+                    if bounding_box[1] > vertices[1]: bounding_box[1] = vertices[1]
+                    if bounding_box[2] > vertices[2]: bounding_box[2] = vertices[2]
+                    if bounding_box[3] < vertices[0]: bounding_box[3] = vertices[0]
+                    if bounding_box[4] < vertices[1]: bounding_box[4] = vertices[1]
+                    if bounding_box[5] < vertices[2]: bounding_box[5] = vertices[2]
+        f.close()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import mean
 import mesh_io
 import renderer
 import filter_io
@@ -7,5 +8,7 @@ meshes = mesh_io.get_all_meshes(mesh_files)
 
 filter_io.get_class(mesh_files)
 filter_io.get_faces_vertices(mesh_files)
+filter_io.get_face_type(mesh_files)
+filter_io.get_bounding_box(mesh_files)
 
 renderer.render_meshes(meshes[5:20])
