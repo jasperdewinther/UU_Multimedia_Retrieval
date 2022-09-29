a877a8a3da19133fab3be043279d8c3df55ae493
jasperdewinther <jaspersophie10@gmail.com>
2022-09-29 13:35:47 +0200

    fixed runtime errors

--- descriptors.py
+++ descriptors.py
@@ -5,18 +5,19 @@ import ast
 import pandas as pd
 import os
 
-def get_global_descriptors(meshes):
+
+def get_global_descriptors(meshes: list[mesh_data.MeshData]):
     # finds the surface area, compactness, axis-aligned boudning-box volume, diameter and eccentricity
     counter = 0
     for mesh in meshes:
         # surface area
-        surface_area = mesh.area
+        surface_area = mesh.trimesh_data.area
 
-        #compactness
-        volume = mesh.volume
+        # compactness
+        volume = mesh.trimesh_data.volume
         compactness = surface_area**3 / (36*math.pi*(volume)**2)
 
-        #axis-aligned bounding-box volume
+        # axis-aligned bounding-box volume
         data = pd.read_csv(os.getcwd() + '/faces_vertices.csv', sep=',')
         bounding_box = data['bounding box'][counter]
         boxList = ast.literal_eval(bounding_box)
@@ -28,8 +29,9 @@ def get_global_descriptors(meshes):
         z2 = float(boxList[5])
         bb_volume = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
-        #diameter
-        diameter = math.max(x1 - x2, y1 - y2, z1 - z2)
+        # diameter
+        diameter = max(x1 - x2, y1 - y2, z1 - z2)
 
-        #eccentricity
-        print(surface_area + ", " + compactness + ", " + bb_volume + ", " + diameter)
+        # eccentricity
+        print(str(surface_area) + ", " + str(compactness) +
+              ", " + str(bb_volume) + ", " + str(diameter))
