380c8fad370bd71340a91175e365a29143cf91d6
ArjenSimons <arjen.simons@gmail.com>
2022-09-30 14:57:57 +0200

    Add function that computes the eigenvalues and eigenvectors

--- normalization.py
+++ normalization.py
@@ -1,5 +1,6 @@
 import mesh_data
 from trimesh import Trimesh
+import numpy as np
 
 def GetBaryCenter(mesh: Trimesh):
     print(mesh.center_mass)
@@ -28,3 +29,19 @@ def NormalizeScale(meshes: list[mesh_data.MeshData]):
         scale_factor = 1 / GetBoundingBoxBiggestAxis(mesh.bounding_box)
         for vertex in mesh.trimesh_data.vertices:
             vertex *= scale_factor
+
+def GetEigenValuesAndVectors(mesh: Trimesh):
+    n_points = len(mesh.vertices)
+    x_coords = [vertex[0] for vertex in mesh.vertices]
+    y_coords = [vertex[1] for vertex in mesh.vertices]
+    z_coords = [vertex[2] for vertex in mesh.vertices]
+    A = np.zeros((3, n_points))
+    A[0] = x_coords
+    A[1] = y_coords
+    A[2] = z_coords
+
+    A_cov = np.cov(A)
+
+    eigenvalues, eigenvectors = np.linalg.eig(A_cov);
+
+    return eigenvalues, eigenvectors
