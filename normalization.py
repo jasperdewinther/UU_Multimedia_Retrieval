a43c0a3600a36b20ad71baeb7ae28309b98fbf32
ArjenSimons <arjen.simons@gmail.com>
2022-09-30 10:50:03 +0200

    Add normalization functions

---
+++ normalization.py
@@ -0,0 +1,24 @@
+import mesh_data
+
+def GetBaryCenter(mesh: mesh_data.MeshData):
+    print(mesh.trimesh_data.center_mass)
+    return mesh.center_mass
+
+def NormalizeTranslations(meshes: list[mesh_data.MeshData]):
+    baryCenter = GetBaryCenter
+    for mesh in meshes:
+        for vertex in mesh.trimesh_data.vertices:
+            vertex -= baryCenter
+
+def GetBoundingBoxBiggestAxis(boundingbox):
+    Dx = boundingbox[0] - boundingbox[3]
+    Dy = boundingbox[1] - boundingbox[4]
+    Dz = boundingbox[2] - boundingbox[5]
+
+    return max(Dx, Dy, Dz)
+
+def NormalizeScale(meshes: list[mesh_data.MeshData]):
+    for mesh in meshes:
+        scale_factor = 1 / GetBoundingBoxBiggestAxis(mesh.bounding_box)
+        for vertex in mesh.trimesh_data.vertices:
+            vertex *= scale_factor
